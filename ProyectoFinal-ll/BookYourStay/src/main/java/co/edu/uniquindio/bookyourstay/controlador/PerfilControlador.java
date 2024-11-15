3bf80a63958cb2803ee48ae73e5c5c23b57e9835
AngelicaReyesB <angelicam.reyesb@uqvirtual.edu.co>
2024-11-15 13:32:37 -0500

    .

--- ProyectoFinal-ll/BookYourStay/src/main/java/co/edu/uniquindio/bookyourstay/controlador/PerfilControlador.java
+++ ProyectoFinal-ll/BookYourStay/src/main/java/co/edu/uniquindio/bookyourstay/controlador/PerfilControlador.java
@@ -5,8 +5,47 @@ import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class PerfilControlador implements Observable, Initializable {
+
+    @FXML private Button btnActualizarDatos;
+    @FXML private Button btnBilleteraVirtual;
+    @FXML private Button btnCancelarReserva;
+    @FXML private Button btnEliminarCuenta;
+    @FXML private Button btnRegresar;
+    @FXML private Button btnVerDetalles;
+    @FXML private ListView<?> listaReservas;
+
+
+
+    @FXML void detallesAlojamiento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarCuenta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irActualizarDatosCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irBilleteraVirtual(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irPanelUsuario(ActionEvent event) {
+
+    }
+
     @Override
     public void notificar() {
 
